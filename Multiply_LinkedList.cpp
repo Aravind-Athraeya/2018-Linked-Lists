b652359ec383058958ec47a4101c6873ac9ccd55
Aravind <madrasmetallurgist@gmail.com>
2016-10-22 19:54:57 +0530

    Add files via upload
    No bariner :| Multiplying two linked lists

---
+++ Multiply_LinkedList.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <string> 
+#include<conio.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include <queue>
+using namespace std;
+
+
+typedef struct ListNode
+{
+	int value;
+	struct ListNode *Next;
+}node;
+
+
+
+void Push(node** head_ref, int new_data)
+{
+	node* new_node = new node();	
+	new_node->value = new_data;	
+
+	
+
+	/* link the old list off the new node */
+	new_node->Next = (*head_ref);
+
+	/* move the head to point to the new node */
+	(*head_ref) = new_node;
+
+
+}
+
+
+
+
+
+	
+ int GetConsoloflist(node * lst)
+{
+	 int consol = 0 ;
+	
+	 while (lst != NULL)
+	 {
+		 consol = consol * 10 + lst->value;
+		 lst = lst->Next;
+	 }
+
+
+
+
+	 return consol;
+}
+
+
+int main()
+{
+	node *lstOne = NULL;
+	node *lndTwo = NULL;
+	Push(&lstOne, 2);
+	Push(&lstOne, 3);
+	Push(&lndTwo, 2);
+	int lstOneSum = GetConsoloflist(lstOne); 
+	int lstTwoSum = GetConsoloflist(lndTwo);
+
+	int total = lstOneSum * lstTwoSum;
+	return 0;
+
+}
+
+
+
+
+
+
