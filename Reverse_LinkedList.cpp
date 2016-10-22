8e364894bc909963332d6e49dc685a1856518d5d
Aravind <madrasmetallurgist@gmail.com>
2016-10-22 21:56:12 +0530

    Reversing Linked List

---
+++ Reverse_LinkedList.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <string> 
+#include<conio.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include <queue>
+using namespace std;
+
+
+typedef struct ListNode
+{
+	int value;
+	struct ListNode *Next;
+}node;
+
+
+
+void Push(node** head_ref, int new_data)
+{
+	node* new_node = new node();	
+	new_node->value = new_data;	
+
+	
+
+	/* link the old list off the new node */
+	new_node->Next = (*head_ref);
+
+	/* move the head to point to the new node */
+	(*head_ref) = new_node;
+
+
+}
+
+
+
+
+
+	
+ void reverse(node ** lst)
+{
+	 node* prev = NULL;
+	 node* curr = (*lst);
+	 node* next;
+	 while (curr != NULL)
+	 {
+
+		 next = curr->Next;
+		 curr->Next = prev;
+		 prev = curr;
+		 curr = next;
+	 }
+	 *lst = prev;
+
+}
+
+
+int main()
+{
+	node *lstOne = NULL;
+	//node *lndTwo = NULL;
+	Push(&lstOne, 1);
+	Push(&lstOne, 2);
+	Push(&lstOne, 3);
+	Push(&lstOne, 4);
+	reverse(&lstOne);
+	node *ldstOne = lstOne;
+
+	return 0;
+
+}
+
+
+
+
+
+
